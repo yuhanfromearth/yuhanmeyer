e51cb8b9082ae6a7eef53ae8b81e5470433ae484
ChiefYuHan <mail.yuhanmeyer@gmail.com>
2025-01-04 15:45:35 +0100

    setup vite, react, ts, tailwindcss

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "tailwindcss";
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
